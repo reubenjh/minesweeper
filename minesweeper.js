cb5b98a4ab5c1fc569fc99f4891eb7e780ddc697
reubenjh <rjharcourt@gmail.com>
2018-04-24 13:10:11 +1200

    made function to create the board. currently makes everything a bomb and board size = 5 x 5

--- minesweeper.js
+++ minesweeper.js
@@ -2,6 +2,8 @@ document.addEventListener('DOMContentLoaded', startGame)
 
 // Define your `board` object here!
 var board = {
+  /* comment muting manual board setup
+
   cells: [
     {
       row:0,
@@ -67,10 +69,14 @@ var board = {
       hidden:true
     }
   ]
+  */
 };
 
 function startGame () {
   // Don't remove this function call: it makes the game work!
+  // calling makeBoard first to generate the contents of the board object.
+  makeBoard ()
+
   for (var i = 0; i < board.cells.length; i++) {
     var surroundingMines = countSurroundingMines(board.cells[i]);
     board.cells[i].surroundingMines = surroundingMines;
@@ -101,6 +107,51 @@ function checkForWin () {
   // detected that they've won, that is!)
   lib.displayMessage('You win!')
 }
+
+// Defining makeBoard
+function makeBoard () {
+  // Setting board size
+  board.width = 5;
+  board.height = 5;
+  board.area = board.width * board.height;
+  
+  // Setting board cells
+  board.cells = [];
+  for (cell = 0; cell < board.area; cell++) {
+    board.cells.push({});
+  }
+
+  // Initializing row and col variables
+  let myRow = 0;
+  let myCol = 0;
+
+  // Looping through cell objects and assigning row and col properties.
+  // Only works up to width * 5 since that's all I needed. 
+  for (cell = 0; cell < board.cells.length; cell++) {
+    if (cell == board.width || cell == (board.width * 2) || cell == (board.width * 3) || cell == (board.width * 4) || cell == (board.width * 5)) {
+      myRow++;
+      myCol = myCol - board.width;
+    }
+
+    board.cells[cell].row = myRow;
+    board.cells[cell].col = myCol;
+    console.log('the current cell is ' + board.cells[cell])
+    myCol++;
+  }
+
+  // Looping through cell objects and assigning hidden, isMine and isMarked
+  // properties.
+  for (cell = 0; cell < board.cells.length; cell++) {
+    board.cells[cell].hidden = true;
+    board.cells[cell].isMine = true;
+    board.cells[cell].isMarked = false;
+  }
+
+
+
+}
+
+
 
 // Define this function to count the number of mines around the cell
 // (there could be as many as 8). You don't have to get the surrounding
